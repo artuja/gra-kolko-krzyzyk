cd27b73a45533fe25f3d4ae6e5679e2f2b90b936
Artur Jasionowski <artuja@wp.pl>
2021-12-18 09:55:27 +0100

    Plansza do gry

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # Tutaj będzie głowny kod gry
+ 
+ # Definicja tablicy - plansza do gry
+PlanszaDoGry = {'7': ' ', '8': ' ', '9': ' ',
+                '4': ' ', '5': ' ', '6': ' ',
+                '1': ' ', '2': ' ', '3': ' '}

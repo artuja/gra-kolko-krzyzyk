cd85470f174882a0cb236dcd40c2f6de33117ff7
Artur Jasionowski <artuja@wp.pl>
2021-12-18 10:30:04 +0100

    skonczona plansza do gry

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ KlawiszeGry = []
 for key in PlanszaDoGry:
     KlawiszeGry.append(key)
 
+
+# Drukuj plansze do gry
+def DrukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
+
+DrukujPlansze(PlanszaDoGry)
